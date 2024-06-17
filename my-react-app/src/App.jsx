aec73d150bc2e376f67ec3b956365e7ac4ba04d6
Sri-Kesavan07 <realmr.k7@gmail.com>
2024-06-17 21:15:54 +0530

    first commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -25,35 +25,33 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div className="app">
-        <h1>MovieWatch</h1>
-        <div className="search">
-          <input
-            placeholder="Search for movies"
-            value={searchTerm}
-            onChange={(event) => setSearchTerm(event.target.value)}
-          />
-          <img
-            src={SearchIcon}
-            alt="Search"
-            onClick={() => searchMovies(searchTerm)}
-          />
-        </div>
-
-        {movies?.length > 0 ? (
-          <div className="container">
-            {movies.map((movie) => (
-              <MovieCard movie={movie} />
-            ))}
-          </div>
-        ) : (
-          <div className="empty">
-            <h2>No movies found</h2>
-          </div>
-        )}
+    <div className="app">
+      <h1>MovieWatch</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="Search"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
-    </>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
   );
 }
 
